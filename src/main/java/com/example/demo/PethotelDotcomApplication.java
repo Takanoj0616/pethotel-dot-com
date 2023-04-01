9d8e2c92159d188aba634832df42b1657511501d
Takanoj0616 <takanoj19920616@gmail.com>
2023-04-01 15:48:50 +0900

    first commit

---
+++ src/main/java/com/example/demo/PethotelDotcomApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PethotelDotcomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PethotelDotcomApplication.class, args);
+	}
+
+}
