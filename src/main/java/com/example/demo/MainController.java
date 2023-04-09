2871bed8cd86d3c754d995d73265f5fed3d6ecba
Takanoj0616 <takanoj19920616@gmail.com>
2023-04-09 14:17:13 +0900

    更新

--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -1,15 +1,42 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.demo.model.Person;
+import com.example.demo.repository.PersonRepository;
 
 @Controller
 public class MainController {
 
-	@GetMapping("/index")
-	public String write1(Model model) {
-		model.addAttribute("moji", "hello world!");
+	private final PersonRepository repository;
+
+	@Autowired
+    public BaseController(PersonRepository repository){
+        this.repository = repository;
+    }
+
+	@GetMapping("/")
+	public String write1() {
 		return "index";
+	}
+
+	@GetMapping("/item")
+	public String home(@ModelAttribute Person person) {
+		return "item";
+	}
+
+	@PostMapping("/form")
+	public String result(@Validated @ModelAttribute Person person, BindingResult result) {
+		if (result.hasErrors()) {
+			return "form";
+		}
+		repository.save(person);
+		return "result";
 	}
 }
---
+++ src/main/java/com/example/demo/model/Person.java
@@ -0,0 +1,69 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Entity
+@Data
+public class Person {
+
+	@Id
+	@GeneratedValue
+	private long id;
+
+	/**
+	 * <p>
+	 * 項目名：名前
+	 * </p>
+	 * <p>
+	 * 必須入力かつ、最大10文字まで
+	 * </p>
+	 */
+	@NotBlank
+	@Size(max = 10)
+	private String name;
+
+	/**
+	 * <p>
+	 * 項目名：年齢
+	 * </p>
+	 * <p>
+	 * 1以上
+	 * </p>
+	 */
+	@NotNull
+	@Min(1)
+	private Integer age;
+
+	/**
+	 * <p>
+	 * 項目名：住所
+	 * </p>
+	 * <p>
+	 * 必須入力かつ、最大10文字まで
+	 * </p>
+	 */
+	@NotBlank
+	@Size(max = 30)
+	private String address;
+
+	/**
+	 * <p>
+	 * 項目名：メールアドレス
+	 * </p>
+	 * <p>
+	 * 条件：必須入力かつ、最大64文字まで
+	 * </p>
+	 */
+	@NotBlank
+	@Email
+	@Size(max = 64)
+	private String email;
+}
