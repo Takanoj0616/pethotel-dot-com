6ffc465341c23b1e8ef6005a7b0513b94cf53ea6
Takanoj0616 <takanoj19920616@gmail.com>
2023-04-20 13:46:01 +0900

    フロント更新

--- src/main/java/com/example/demo/model/Person.java
+++ src/main/java/com/example/demo/model/Person.java
@@ -18,50 +18,18 @@ public class Person {
 	@GeneratedValue
 	private long id;
 
-	/**
-	 * <p>
-	 * 項目名：名前
-	 * </p>
-	 * <p>
-	 * 必須入力かつ、最大10文字まで
-	 * </p>
-	 */
 	@NotBlank
 	@Size(max = 10)
 	private String name;
 
-	/**
-	 * <p>
-	 * 項目名：年齢
-	 * </p>
-	 * <p>
-	 * 1以上
-	 * </p>
-	 */
 	@NotNull
 	@Min(1)
 	private Integer age;
 
-	/**
-	 * <p>
-	 * 項目名：住所
-	 * </p>
-	 * <p>
-	 * 必須入力かつ、最大10文字まで
-	 * </p>
-	 */
 	@NotBlank
 	@Size(max = 30)
 	private String address;
 
-	/**
-	 * <p>
-	 * 項目名：メールアドレス
-	 * </p>
-	 * <p>
-	 * 条件：必須入力かつ、最大64文字まで
-	 * </p>
-	 */
 	@NotBlank
 	@Email
 	@Size(max = 64)
